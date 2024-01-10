f6adaf809b44abe0229afb90909563b5d3c5cebd
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-10 15:55:34 +0400

    Update loop1.java

--- loop1.java
+++ loop1.java
@@ -1,14 +1,14 @@
-public class loop1{
-	public static void main(String[] arg){
-		for (int a=1; a<=2; a++){
-			for(int b=1; b<=2;){
-				System.out.println(b);
-				break;
-			}
+public class loop1 {
+  public static void main(String[] arg) {
+    for (int a = 1; a <= 2; a++) {
+      for (int b = 1; b <= 2;) {
+        System.out.println(b);
+        break;
+      }
 
-			System.out.println(a);
-			continue;
-		
-		}
-	}
+      System.out.println(a);
+      continue;
+
+    }
+  }
 }
